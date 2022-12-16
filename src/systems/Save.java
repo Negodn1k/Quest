4d53fd6d8bc5ed0ee30570ad79f3a7cafe070289
Negodn1k <havkaset@gmail.com>
2022-12-16 13:41:32 +0300

    Do a save system without a key words

---
+++ src/systems/Save.java
@@ -0,0 +1,25 @@
+package systems;
+
+public class Save {
+    public String keyWord;
+
+    public void saveChoice() {
+        switch (keyWord) {
+            case ("1"):
+
+                break;
+            case ("2"):
+
+                break;
+            case ("3"):
+
+                break;
+            case ("4"):
+
+                break;
+            case ("5"):
+
+                break;
+        }
+    }
+}
