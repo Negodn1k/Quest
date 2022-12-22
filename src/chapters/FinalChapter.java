faf8b0e75714044a5f1a43d722833ba1e34eb00d
Negodn1k <havkaset@gmail.com>
2022-12-22 17:26:45 +0300

    Rename ThirdChapter in FinalChapter, delete old FinalChapter for useless. Add story in FinalChapter

--- src/chapters/FinalChapter.java
+++ src/chapters/FinalChapter.java
@@ -2,22 +2,132 @@ package chapters;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class FinalChapter extends ThirdChapter {
+public class FinalChapter extends SecondChapter {
+    static int firstChoice;
+    static int secondChoice;
+    static boolean endingItem;
+    static boolean firstChoiceCheck = true;        //Проверка правильности выбора для зацикливания выбора, при неправильном выборе цикл повторится
+    static boolean secondChoiceCheck = true;
+
     public static void go() {
         try {
-            FileWriter writer = new FileWriter("Final chapter save.txt");
-            if (endingItem) {
-                System.out.println("Good ending");
-                writer.write("Good ending");
-                writer.close();
-            } else {
-                System.out.println("Bad ending");
-                writer.write("Bad ending");
-                writer.close();
+            FileWriter writer = new FileWriter("Third chapter Save.txt");
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Оставшись под впечатлением, Ержан пошёл дальше уже по тропинке, \n" +
+                    "в начале которой был указатель с надписью \"Город Лирия\"." +
+                    "\nЕржан очень хотел есть и поэтому побыстрее зашагал к городу." +
+                    "\nСпустя какое то время он среди деревьев увидел очертания города." +
+                    "\nЕржан ускорил шаг и уже был у открытых ворот Лирии." +
+                    "\nОн остановился у указателя на развилке.\n");
+
+            while (firstChoiceCheck) {
+                System.out.println("\n1. Спросить дорогу у стражников\n2. Пойти корчму\n3. Пойти на рынок");
+                firstChoice = input.nextInt();
+                switch (firstChoice) {
+                    case (2):
+                        System.out.println("\nЕржан пошёл в корчму по указателю и через какое то время оказался на месте." +
+                                "\nКогда он вошёл туда, то услышал красивую музыку, весёлую обстановку и запах вкусных бургеров и шаурмы");                //Развитие сюжета после первой развилки(Первый выбор)
+
+                        firstChoiceCheck = false;
+                        writer.write("Третья глава \nПервый выбор: Пойти в корчму");
+
+                        while (secondChoiceCheck) {
+                            System.out.println("\n1. Шаурма\n2. Бургер\n");
+
+                            secondChoice = input.nextInt();
+                            switch (secondChoice) {
+                                case (2):
+                                    System.out.println("Ержан заказал бургер. Ему его приготовили и он принялся его поглощать." +
+                                            "\nПочувствовав вкус бургера, Ержан проснулся в холодной темнице и рядом никого не было." +
+                                            "\nТак и закончилась история");        // Развитие сюжета после второй развилки(первый выбор)
+
+                                    secondChoiceCheck = false;
+                                    writer.write("\nВторой выбор: Бургер");
+                                    writer.close();
+                                    break;
+                                case (1):
+                                    System.out.println("Он заказал шаурму. Ему её приготовили и он принялся её поглощать." +
+                                            "\nПочувствовав вкус шаурмы, Ержан проснулся у себя дома и понял что это был всё сон. " +
+                                            "\nОн понял что ему нужно побольше работать чтобы было больше денег на все нужды");        // Развитие сюжета после второй развилки(второй выбор)
+
+                                    secondChoiceCheck = false;
+                                    endingItem = true;
+                                    writer.write("Второй выбор: Шаурма");
+                                    writer.close();
+                                    break;
+                                default:
+                                    System.out.println("Такой выбор не возможен! Повторите попытку");
+
+                                    secondChoiceCheck = true;
+                                    writer.write("Ошибка выбора");
+                                    break;
+                            }
+                        }
+                        break;
+                    case (3):
+                        System.out.println("Голод взял верх над главным героем и он пошёл на рынок." +
+                                "\nТам он увидел заведение Аль-шарк, но там был скудный выбор." +
+                                "\nБыли лишь шаурма и бургеры ");                // Развитие сюжета после первой развилки(второй выбор)
+                        firstChoiceCheck = false;
+                        writer.write("Третья глава\nПервый выбор:");
+
+                        while (secondChoiceCheck) {
+
+                            System.out.println("\n1. Шаурма\n2. Бургер\n");
+                            secondChoice = input.nextInt();
+
+                            switch (secondChoice) {
+                                case (2):
+                                    System.out.println("Ержан заказал бургер. Ему его приготовили и он принялся его поглощать." +
+                                            "\nПочувствовав вкус бургера, Ержан проснулся в холодной темнице и рядом никого не было." +
+                                            "\nТак и закончилась история");        // Развитие сюжета после второй развилки(первый выбор)
+
+                                    secondChoiceCheck = false;
+                                    writer.write("\nВторой выбор: Бургер");
+                                    writer.close();
+                                    break;
+                                case (1):
+                                    System.out.println("Он заказал шаурму. Ему её приготовили и он принялся её поглощать." +
+                                            "\nПочувствовав вкус шаурмы, Ержан проснулся у себя дома и понял что это был всё сон. " +
+                                            "\nОн понял что ему нужно побольше работать чтобы было больше денег на все нужды");        // Развитие сюжета после второй развилки(второй выбор)
+
+                                    secondChoiceCheck = false;
+                                    endingItem = true;
+                                    writer.write("Второй выбор: Шаурма");
+                                    writer.close();
+                                    break;
+                                default:
+                                    System.out.println("Такой выбор не возможен! Повторите попытку");
+
+                                    secondChoiceCheck = true;
+                                    writer.write("Ошибка выбора");
+                                    break;
+                            }
+                        }
+                        break;
+                    case (1):
+                        System.out.println("Ержан не стал доверять указателю и решил спросить дорогу у стражников." +
+                                "\nНо как только он подошёл к ним, они тут же спросили его имя и не зная такого," +
+                                "\nувели его с собой для разбирательств, но это ничего не дало." +
+                                "\nЕржан так и сгнил в темнице...");                // Развитие сюжета после первой развилки(третий выбор)
+
+                        firstChoiceCheck = false;
+                        writer.write("Третья глава\nПервый выбор: Спросить дорогу у стражников");
+                        writer.close();
+                        break;
+                    default:
+                        System.out.println("Такой выбор не возможен! Повторите попытку");
+
+                        writer.write("\nОшибка выбора");
+                        break;
+                }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 }
+
--- src/systems/ChapterSelect.java
+++ src/systems/ChapterSelect.java
@@ -1,9 +1,8 @@
 package systems;
 
-import chapters.FinalChapter;
 import chapters.FirstChapter;
 import chapters.SecondChapter;
-import chapters.ThirdChapter;
+import chapters.FinalChapter;
 
 import java.util.Scanner;
 
@@ -12,23 +11,20 @@ public class ChapterSelect extends FinalChapter {
 
     public static void go(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Выберите главу: \n1. Первая глава \n2. Вторая глава \n3. Третья глава");
+        System.out.println("Выберите главу: \n1. Первая глава \n2. Вторая глава \n3. Финальная глава");
         selectNum = input.next();
 
         switch (selectNum) {
             case ("1"):
                 FirstChapter.go();
                 SecondChapter.go();
-                ThirdChapter.go();
                 FinalChapter.go();
                 break;
             case ("2"):
                 SecondChapter.go();
-                ThirdChapter.go();
                 FinalChapter.go();
                 break;
             case ("3"):
-                ThirdChapter.go();
                 FinalChapter.go();
                 break;
         }
